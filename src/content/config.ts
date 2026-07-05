bc634829af289c436fabb1ae41db5cb38d570fab
Rob Trube <rtrube@strategysimple.com>
2026-07-05 16:49:38 -0400

    Update blog collection schema and loader

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,14 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    date: z.date(),
+    date: z.coerce.date(),
     category: z.string(),
-    tags: z.array(z.string()),
+    tags: z.array(z.string()).default([]),
     author: z.string().default('modus6'),
   }),
 });
